4f6fc903c532a30e65da8d2338f4d451b157970f
rortizs <rortizs@miumg.edu.gt>
2020-04-10 18:23:29 -0600

    agregando plugins, header,footer, listado

---
+++ vistas/footer.php
@@ -0,0 +1,21 @@
+<footer class="main-foooter">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 1.0
+    </div>
+
+    <strong>Cpyright &copy; 2020 COVID-19 <a href="curl https://corona-stats.online/">SISTEMA UMG</a></strong> All rights reservados.
+    </footer>
+
+    <!--JQUERY-->
+    <script src="../public/js/jquery-3.1.1.min.js"></script>
+    <script src="../public/datatables/dataTables.buttons.min.js"></script>
+    <script src="../public/datatables/buttons.html5.min.js"></script>
+    <script src="../public/datatables/buttons.colVis.min.js"></script>
+    <script src="../public/datatables/jszip.min.js"></script>
+    <script src="../public/datatables/pdfmake.min.js"></script>
+    <script src="../public/datatables/vfs_fonts.js"></script> 
+
+    <script src="../public/js/bootbox.min.js"></script> 
+    <script src="../public/js/bootstrap-select.min.js"></script>  
+    </body>
+    </html>
---
+++ vistas/header.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>SISTEMA VENTAS | UMG ANALISIS DE SISTEMAS</title>
+
+    <!--plugins necesarios de nuestra plantilla-->
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.5 -->
+    <link rel="stylesheet" href="../public/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../public/css/font-awesome.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../public/css/AdminLTE.min.css">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+         folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="../public/css/_all-skins.min.css">
+    <link rel="apple-touch-icon" href="../public/img/apple-touch-icon.png">
+    <link rel="shortcut icon" href="../public/img/favicon.ico">
+
+    <!-- DATATABLES -->
+    <link rel="stylesheet" type="text/css" href="../public/datatables/jquery.dataTables.min.css">    
+    <link href="../public/datatables/buttons.dataTables.min.css" rel="stylesheet"/>
+    <link href="../public/datatables/responsive.dataTables.min.css" rel="stylesheet"/>
+
+    <link rel="stylesheet" type="text/css" href="../public/css/bootstrap-select.min.css">
+
+    </head>
+    <!--ESTA CLASE ME PERMITE CAMBIAR EL COLOR DE MI PLANTILLA-->
+    <body class="hold-transition skin-blue-light sidebar-mini">
+        <div class="wrapper">
+
+        <header class="main-header">
+
+        <!--logo-->
+        <a href="index2.html" class="logo">
+
+            <span class="logo-mini"><b>UMG</b>SISTEMA</span>
+
+            <span class="logo-lg"><b>UMG ANALISIS 1</b></span>
+
+        
+        </a>
+
+        <nav class="navbar navbar-static-top" role="navigation">
+        
+            <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+                
+                <span class="sr-only">Navegacion</span>
+
+            
+            </a>
+
+            <div class="navbar-custom-menu">
+            
+                <ul class="nav navbar-nav">
+                
+                    <li class="dropdown user user-menu">
+                    
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        
+                            <img src="../public/dist/img/user2-160x160.jpg" class="user-image"  alt="User Image">
+                            <span class="hidden-xs">Richard Ortiz</span>
+                        </a>
+                        <ul class="dropdown-menu">
+                        
+                            <li class="user-header">
+
+                                <img src="" alt="">
+
+                                <p>
+                                
+                                CUALQUIER COSA
+                                <small>www.umg.edu.gt</small>
+                                </p>
+                            
+                            </li>
+
+                            <li class="user-footer">
+
+                                    <div class="pull-right">
+                                        <a href="#" class="btn btn-default btn-flat">cerrar</a>
+                                    </div>
+
+                            </li>
+                        </ul>
+                    </li>
+                
+                </ul>
+            
+            </div>
+        
+        </nav>
+        </header>
+
+        <aside class="main-sidebar">
+            <section class="sidebar">
+            <ul class="sidebar-menu">
+            
+            
+            
+            </ul>
+
+            </section>
+        </aside>
+
+
+        
+        
+        
+        
+        
+        
